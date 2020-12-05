b42b143e02e4a7dbd894f533958ce65bd874954c
Ali Nazari <ms.kinect@yahoo.com>
2020-12-05 14:03:51 +0330

    Update main.js

--- playground/change-background/main.js
+++ playground/change-background/main.js
@@ -1,5 +1,5 @@
 document.getElementsByTagName('button')[0].onclick = function() {
-    const colors = ['#1abc9c', '#e67e22', '#2c3e50', '#8e44ad', '#95a5a6', '#f1c40f', '#fd79a8', '#00cec9', '#fdcb6e']
+    const colors = ['#1abc9c', '#e67e22', '#2c3e50', '#8e44ad', '#95a5a6', '#f1c40f', '#fd79a8', '#00cec9', '#fdcb6e', '#ff5252']
 
     const randomIndex = Math.floor(Math.random() * colors.length);
     const color = colors[randomIndex];
