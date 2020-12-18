d87221f6eb0618aef1cc622e47427f1fc0396b5a
Ali Nazari <ms.kinect@yahoo.com>
2020-12-18 18:38:11 +0330

    Update main.js

--- playground/counter/main.js
+++ playground/counter/main.js
@@ -1,15 +1,15 @@
 document.querySelector('.increase').onclick = function() {
   let oldNumber = document.getElementById('number').innerHTML;
   oldNumber = parseInt(oldNumber);
-  oldNumber = oldNumber + 1;
+  let newNumber = oldNumber + 1;
 
-  document.getElementById('number').innerHTML = oldNumber;
+  document.getElementById('number').innerHTML = newNumber;
 }
 
 document.querySelector('.decrease').onclick = function() {
   let oldNumber = document.getElementById('number').innerHTML;
   oldNumber = parseInt(oldNumber);
-  oldNumber = oldNumber - 1;
+  let newNumber = oldNumber - 1;
 
-  document.getElementById('number').innerHTML = oldNumber;
+  document.getElementById('number').innerHTML = newNumber;
 }
