f2abfbe9f606ce0d00154150e6f10ef8cc2e4d29
Ali Nazari <anap1899@gmail.com>
2021-12-29 14:21:19 +0330

    Update main.js

--- playground/change-background/js/main.js
+++ playground/change-background/js/main.js
@@ -11,34 +11,34 @@ const colors = [
   'indigo',
 ];
 
+function setColor(color) {
+  document.body.style.backgroundColor = color;
+  document.querySelector('#current-color').innerHTML = color;
+
+  const currentActiveButton = document.querySelector('button.active');
+
+  if (currentActiveButton !== null) {
+    currentActiveButton.classList.remove('active');
+  }
+
+  document.querySelector(`button[data-color=${color}]`).classList.add('active');
+}
+
 colors.forEach(color => {
-  const element = document.createElement('button');
-  element.setAttribute('data-color', color);
-  element.style.backgroundColor = color;
-  element.onclick = function () {
+  const button = document.createElement('button');
+  button.style.backgroundColor = color;
+  button.setAttribute('data-color', color);
+
+  button.onclick = function () {
     setColor(color);
   }
 
-  document.querySelector('div.colors').appendChild(element);
+  document.querySelector('div.colors').appendChild(button);
 });
 
-
-function randomBackgroundColor() {
+document.body.onload = function () {
   const randomIndex = Math.floor(Math.random() * colors.length);
   const color = colors[randomIndex];
+
   setColor(color);
 }
-
-function setColor(color) {
-  document.body.style.backgroundColor = color;
-  document.querySelector('#current-color').innerHTML = color.toUpperCase();
-
-  const activeColor = document.querySelector('div.colors button.active');
-  if (activeColor) {
-    activeColor.classList.remove('active');
-  }
-
-  document.querySelector(`div.colors button[data-color="${color}"]`).classList.add('active');
-}
-
-document.body.onload = randomBackgroundColor;
