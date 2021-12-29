5928c5ad4bd121b17706172fbd07352f9a4866a2
Ali Nazari <anap1899@gmail.com>
2021-12-29 13:49:01 +0330

    Delete playground/change-background directory

--- playground/change-background/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementsByTagName('button')[0].onclick = function() {
-    const colors = ['#1abc9c', '#e67e22', '#2c3e50', '#8e44ad', '#95a5a6', '#f1c40f', '#fd79a8', '#00cec9', '#fdcb6e', '#ff5252']
-
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    const color = colors[randomIndex];
-
-    document.getElementsByTagName('body')[0].style.backgroundColor = color
-}
